d405302b6556456f5077db1b9a8df4c90e506789
tmunawwer <37419565+tmunawwer@users.noreply.github.com>
2018-06-16 15:32:48 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,11 +6,13 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'm!test') {
+    	message.reply('tested 1');
+  	}
+    if (message.content === 'm!help') {
+    	message.reply(':V Lol under development');
   	}
 });
 
 // THIS  MUST  BE  THIS  WAY
 client.login('NDU3NjIwNDY3NzA5MTE2NDU2.DgbwUw.7AQNTeFaXVJU73IKkKfv0js7N0I');
-    //NDU3NjIwNDY3NzA5MTE2NDU2.DgbwUw.7AQNTeFaXVJU73IKkKfv0js7N0I);
